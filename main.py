8a5aaccb185bd8160cbe1b1861f75ef6d225df19
Egor Vasko <sqz@mail.ru>
2019-11-02 07:11:53 +0400

    working json

--- main.py
+++ main.py
@@ -2,17 +2,27 @@
 # ############################
 # My first parser
 # v0.01
-# to do:
+# to do:    - json to def
 #           - check price changes
-#
-# ##########################
+#           - optimize remove weak
+# ############################
 
 from urllib import request
 from parcerfunc import *
 import os
+import json
+import re
+import time
+import datetime
+t1 = time.time()
+today = datetime.datetime.today()
+time_of_parse = today.strftime("%Y-%m-%d %H:%M:%S")
 
 # ------------------------------------ Variables --------------------------------------
-
+looking_for_link = 'a href="https://dubai.dubizzle.com/motors/used-cars/'
+carslinks = []
+dict_new_raw = {}
+dictnow = {}
 infi = 'https://dubai.dubizzle.com/motors/used-cars/infiniti/gseries/?price__gte=&price__lte=25000&year__gte=2009&year__lte=&kilometers__gte=&kilometers__lte=230000'
 x3 = 'https://dubai.dubizzle.com/motors/used-cars/bmw/x3/?price__gte=&price__lte=25000&year__gte=2007&year__lte=&kilometers__gte=&kilometers__lte=230000'
 bmw3 = 'https://dubai.dubizzle.com/motors/used-cars/bmw/3-series/?price__gte=&price__lte=25000&year__gte=2007&year__lte=&kilometers__gte=&kilometers__lte=230000'
@@ -25,17 +35,21 @@ is3 = 'https://dubai.dubizzle.com/motors/used-cars/lexus/is-series/?price__gte=&
 z350 = 'https://dubai.dubizzle.com/motors/used-cars/nissan/350z/?price__gte=&price__lte=29000&year__gte=2006&year__lte=&kilometers__gte=&kilometers__lte=230000'
 bmw5 = 'https://dubai.dubizzle.com/motors/used-cars/bmw/5-series/?price__gte=&price__lte=25000&year__gte=2004&year__lte=&kilometers__gte=&kilometers__lte=230000'
 cars = [infi, x3, bmw3, bmw1, a4, a5, is1, is2, is3, z350, bmw5]
-#cars = [x3]#test cars for faster parcing
-carslinks = []
-cardict = {}
-looking_for_link = 'a href="https://dubai.dubizzle.com/motors/used-cars/'
+#cars = [bmw3]#test cars for faster parcing
+
+dictold = {}
+dictred ={}
+dict_new = {}
+dict_nochanges = {}
+dict_disc_again = {}
+dict_disc = {}
+dict_red_nochanges = {}
 
 #----------------------------------------------Functions ---------------------------------------------
 
 
 def parse():
-    temp = open(tempfile, mode = 'w+', encoding ='utf-8')
-    temp.truncate(0)
+    linkfound = []
     for i in range(0,len(carslinks)):
         otvet = request.urlopen(carslinks[i])
         mytext1 = otvet.readlines()
@@ -44,30 +58,125 @@ def parse():
                 line_link = str(line)
                 line_link = (line_link[line_link.find('"') + 1 : ])
                 line_link = (line_link[0: + line_link.find('?back')])
-                temp.write(str(line_link) + "\n")
-                z = 0
-                for line in mytext1[num:]:
-                    if "AED" in line.decode('utf-8') and z == 0:
-                        line_price = str(line)
-                        line_price = (line_price[line_price.find('AED') : line_price.find('AED') + 10])
-                        z = 1
-                if line_link.encode('utf-8') not in cardict.keys():
-                    cardict[line_link] = line_price
-    temp.close()
+                if line_link not in linkfound:
+                    linkfound.append(line_link)
+                    z = 0
+                    for line in mytext1[num:]:
+                        if "AED" in line.decode('utf-8') and z == 0:
+                            line_price = str(line)
+#                            line_price = (line_price[line_price.find('AED') : line_price.find('AED') + 10])
+                            line_price = (line_price[line_price.find('AED') + 3 : line_price.find('AED') + 10])
+                            line_price = re.sub(r'\D', '', line_price)
+                            z = 1
+                    if line_link.encode('utf-8') not in dict_new_raw.keys():
+                        dict_new_raw[line_link] = line_price
+
+
+def remove_weak_dict():
+    for i in dict_new_raw:
+        if "116" in str(i) or "118" in str(i) or "120" in str(i) or "316" in str(i) or "320" in str(i) or "323" in str(i) or "325" in str(i) or "250" in str(i):
+            continue
+        else:
+            dictnow[i] = dict_new_raw[i]
+
+
+#def open_json():
+try:
+    with open("data.json", "r",encoding='utf-8') as old_file:
+        dictold = json.load(old_file)
+    with open("data_disc.json", "r", encoding='utf-8') as disc_file:
+        dictred = json.load(disc_file)
+except:
+    dictold = {}
+    dictred = {}
+
+
+def compare():
+    for i in dictnow:
+        if i in dictred:
+            if dictnow[i] < dictred[i]:
+                dict_disc_again[i] = dictnow[i]
+                continue
+            if dictnow[i] >= dictred[i]:
+                dict_red_nochanges[i] = dictnow[i]
+                continue
+        if i in dictold:
+            if dictnow[i] < dictold[i]:
+                dict_disc[i] = dictnow[i]
+                continue
+            if dictnow[i] >= dictold[i]:
+                dict_nochanges[i] = dictnow[i]
+                continue
+        else:
+            dict_new[i] = dictnow[i]
+
+
+def dump_data():
+    out_new = {**dict_nochanges,**dict_new}
+    out_disc = {**dict_disc, **dict_disc_again, **dict_red_nochanges}
+    with open("data_disc.json", "w+") as write_disc_file:
+        json.dump(out_disc, write_disc_file)
+    with open("data.json", "w+") as write_file:
+        json.dump(out_new, write_file)
+
+
+def load_to_html():
+    with open(path + 'output.html', 'w+') as out_file:
+        line = "================================== New cars " + time_of_parse + " ==================================\n<br />"
+        out_file.write(line)
+        for i in dict_new:
+            line = 'Link is : <a href="' + str(i) + '">' + i + '</a>' + "<pre>    Price is: " + str(dict_new[i]) + "</pre><br />"
+            out_file.write(line)
+
+        line = "===================================== Price reduced AGAIN! ===========================================<br />"
+        out_file.write(line)
+        for i in dict_disc_again:
+            line = 'Link is : <a href="' + i + '">' + i + '</a>' + "<pre>    Price is: " + str(dictnow[i]) + "Old price is: " + str(dictred[i]) + "</pre><br />"
+            out_file.write(line)
+
+        line = "========================================= Price reduced ===========================================<br />"
+        out_file.write(line)
+        for i in dict_disc:
+            line = 'Link is : <a href="' + i + '">' + i + '</a>' + "<pre>    Price is: " + str(dict_disc[i]) + "</pre><br />"
+            out_file.write(line)
+
+        line = "========================================= Old Cars ==============================================<br />"
+        out_file.write(line)
+        for i in dict_nochanges:
+            line = 'Link is : <a href="' + i + '">' + i + '</a>' + "<pre>    Price is: " + dict_nochanges[i] + "</pre><br />"
+            out_file.write(line)
+
+        for i in dict_red_nochanges:
+            line = 'Link is : <a href="' + i + '">' + i + '</a>' + "Price is: " + dict_red_nochanges[i] + "<br />"
+            out_file.write(line)
+
+
+def cli_output():
+    for i in dict_new:
+        print("NEW                  Key = " + str(i) + "    Price =     " + str(dictnow[i]))
+
+    for i in dict_disc_again:
+        print("PRICE REDUCED AGAIN! Key = " + str(i) + "    New price = " + str(dictnow[i]) + "    Old price is = " + str(dictred[i]))
+
+    for i in dict_disc:
+        print("PRICE REDUCED!       Key = " + str(i) + "    New price = " + str(dictnow[i]) + "    Old price is = " + str(dictold[i]))
+
+    for i in dict_nochanges:
+        print("OLD                  Key = " + str(i) + "    Value =     " + str(dictnow[i]))
+
+    for i in dict_red_nochanges:
+        print("OLD                  Key = " + str(i) + "    Value =     " + str(dictnow[i]))
 
 
 # ---------------------------------------------main--------------------------------------------------
 
 car_list(cars,carslinks)
 print('Program is running, please wait ~30 seconds')
-parse()              # get all links
-
-for i in cardict:
-    print(i, cardict[i])
-
-remove_weak()        # remove 116, 118, 320, etc..
-rename()             # rename source
-copy_new()           # write from temp1
-copy_old()           # write from source
-exit_procedures()    # closing files
-exit_text()          # Print completion, timing
+parse()                     # get all links
+remove_weak_dict()          # remove 116, 118, 320, etc..
+#open_json()                # load data from files
+compare()                   # compare dictionaries
+dump_data()                 # upload to files
+#cli_output()               # output in command line
+load_to_html()
+exit_text()                 # Print completion, timing
--- parcerfunc.py
+++ parcerfunc.py
@@ -5,12 +5,14 @@ import os
 import getpass
 import platform
 
-
-tempfile = "temp.txt"
-temp1file = "temp1.txt"
 t1 = time.time()
 today = datetime.datetime.today()
 time_of_parse = today.strftime("%Y-%m-%d %H:%M:%S")
+
+
+def car_list(x,y):
+    for car in x:
+        y.append(car)
 
 
 def OSIS(): # define path to Desktop folder
@@ -21,33 +23,6 @@ def OSIS(): # define path to Desktop folder
     else:
         return('')
 path = OSIS()
-
-def car_list(x,y):
-    for car in x:
-        y.append(car)
-
-
-def remove_weak():
-    outputf = "temp1.txt"
-    out_f = open(outputf, mode = 'w+', encoding='utf-8')
-    with open('temp.txt','r+') as in_file, out_f:
-        for line in in_file:
-            if 'g35' in line: continue
-            if '116' in line: continue
-            if '118' in line: continue
-            if '120' in line: continue
-            if '316' in line: continue
-            if '318' in line: continue
-            if '320' in line: continue
-            if '323' in line: continue
-            if '325' in line: continue
-            if '250' in line: continue
-            if '520' in line: continue
-            if '523' in line: continue
-            if '525' in line: continue
-            if '430' in line: continue
-            out_f.write(line)
-    out_f.close()
 
 
 def rename():

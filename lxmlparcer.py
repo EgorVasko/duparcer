22e1c728da3fd294aefed8a0fd2de29de8f62988
Egor Vasko <sqz@mail.ru>
2019-11-15 12:04:51 +0400

    lxml v15-11

--- lxmlparcer.py
+++ lxmlparcer.py
@@ -24,13 +24,13 @@ cars = [infi, x3, bmw3, bmw1, a4, a5, a6, is1, is2, is3, z350, bmw5, clk]
 parsed = []
 carslinks = []
 
-dictold = {}
-dictred ={}
-dict_new = {}
-dict_nochanges = {}
-dict_disc_again = {}
-dict_disc = {}
-dict_red_nochanges = {}
+dictold = []
+dictred =[]
+dict_new = []
+dict_nochanges = []
+dict_disc_again = []
+dict_disc = []
+dict_red_nochanges = []
 
 # =============================== functions
 
@@ -98,53 +98,113 @@ def dubizzle_parse_lxml(base_url):  # main parse
 
 def open_json():
     try:
-        with open("data.json", "r",encoding='utf-8') as old_file:
+        with open("lxml_data.json", "r") as old_file:   #encoding='utf-8'
             dictold = json.load(old_file)
-        with open("data_disc.json", "r", encoding='utf-8') as disc_file:
+        with open("lxml_data_disc.json", "r") as disc_file:   #encoding='utf-8'
             dictred = json.load(disc_file)
     except:
-        dictold = {}
-        dictred = {}
-
-# dictold - parsed before
-# dictred - price was reduced before
-# dictnow = parsed = new parsed cars
-
-for i in parsed:
-    for x in dictred:
-        if i['href'] == x['href']:
-            if i['price'] < x['price']:
-                dict_disc_again.append(i)        # add to again discounted
-                continue
-            if i['price'] >= x['price']:
-                dict_red_nochanges.append(i)
-                continue
-    for z in dictold:
-        if i['price'] < z['price']:
-            dict_disc.append(i)
-            continue
-        if i['price'] >= z['price']:
-            dict_nochanges.append(i)
+        dictold = []
+        dictred = []
 
 
-def compare():
-    for i in dictnow:
-        if i in dictred:
-            if dictnow[i] < dictred[i]:
-                dict_disc_again[i] = dictnow[i]
-                continue
-            if dictnow[i] >= dictred[i]:
-                dict_red_nochanges[i] = dictnow[i]
-                continue
-        if i in dictold:
-            if dictnow[i] < dictold[i]:
-                dict_disc[i] = dictnow[i]
-                continue
-            if dictnow[i] >= dictold[i]:
-                dict_nochanges[i] = dictnow[i]
-                continue
+def compare(list):
+    for i in list:
+        for x in dictred:
+            if i['href'] == x['href']:
+                if i['price'] < x['price']:
+                    dict_disc_again.append(i)        # add to again discounted
+                    continue
+                if i['price'] >= x['price']:
+                    dict_red_nochanges.append(i)
+                    continue
+
+    parsed1 = []
+    for i in dict_disc_again:
+        for x in parsed:
+            if i != x:
+                parsed1.append(x)
+    list = []
+    for i in dict_red_nochanges:
+        for x in parsed1:
+            if i != x:
+                list.append(x)
+    for i in list:
+        for z in dictold:
+            if i['href'] == z['href']:
+                if i['price'] < z['price']:
+                    dict_disc.append(i)
+                    continue
+                if i['price'] >= z['price']:
+                    dict_nochanges.append(i)
+                    continue
+    parsed1 = []
+    for i in dict_disc:
+        for x in list:
+            if i != x:
+                parsed1.append(x)
+    for i in dict_nochanges:
+        for x in parsed1:
+            if i != x:
+                dict_new.append(x)
+
+
+def dump_data():
+    out_new = dict_nochanges + dict_new
+    out_disc = dict_disc + dict_disc_again + dict_red_nochanges
+    with open("lxml_data_disc.json", "w+") as write_disc_file:
+        json.dump(out_disc, write_disc_file)
+    with open("lxml_data.json", "w+") as write_file:
+        json.dump(out_new, write_file)
+
+
+def load_to_html():
+    with open('output.html', 'w+') as out_file:
+        if not dict_new:
+            line = "<center><h2> No new cars " + "</center></h2>"
+            out_file.write(line)
+        if dict_new:
+            line = "<center><h2> New cars " + "</center></h2>"
+            out_file.write(line)
+            for i in dict_new:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+        if not dict_disc_again:
+            line = ""
         else:
-            dict_new[i] = dictnow[i]
+            line = "<center><h2>  Price reduced AGAIN! </center></h2>"
+            out_file.write(line)
+            for i in dict_disc_again:
+                for x in dictred:
+                    if i['href'] == x['href']:
+                        oldprice = x['price']
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "&nbsp;&nbsp;&nbsp;&nbsp;Old price is: " + oldprice + "<br />"
+                out_file.write(line)
+
+        if not dict_disc:
+            line = ""
+        else:
+            line = "<center><h2>  Price reduced </center></h2> "
+            out_file.write(line)
+            for i in dict_disc:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+        if not dict_nochanges:
+            line = ""
+        else:
+            line = "<center><h2>  Old Cars </center></h2>"
+            out_file.write(line)
+            for i in dict_nochanges:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+        if not dict_red_nochanges:
+            line = ""
+        else:
+            for i in dict_red_nochanges:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
 
 # ================================== Execution
 
@@ -160,6 +220,38 @@ result = round(finish - start,2)
 
 print('Parcing completed with lxml method:', result, "seconds\nTotal:" ,len(parsed),"cars parsed")
 
-#for i in parsed:
-#    print(i['title'], i['href'], i['price'], i['year'], i['mileage'])
-#    sheet.append_row([i['title'], i['href'], i['price'], i['year'], i['mileage']])
+for i in parsed:
+    print(i)
+print(dictred)
+print(dictold)
+
+compare(parsed)       # comparing, sorting
+
+print(dict_new)
+print(dict_red_nochanges)
+print(dict_disc_again)
+print(dict_disc)
+print(dict_nochanges)
+
+
+dump_data()     # dumping to base
+load_to_html()  # html
+
+
+def cli_output():
+    for i in dict_new:
+        print("NEW                  Key = " + i['href'] + "    Price =     " + i['price'])
+
+    for i in dict_disc_again:
+        print("PRICE REDUCED AGAIN! Key = " + i['href'] + "    New price = " + i['price'] + "    Old price is = " + i['price'])
+
+    for i in dict_disc:
+        print("PRICE REDUCED!       Key = " + i['href'] + "    New price = " + i['price'] + "    Old price is = " + i['price'])
+
+    for i in dict_nochanges:
+        print("OLD                  Key = " + i['href'] + "    Value =     " + i['price'])
+
+    for i in dict_red_nochanges:
+        print("OLD                  Key = " + i['href'] + "    Value =     " + i['price'])
+
+cli_output()
--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,6 @@
-parsed = [{"href":'href1',"price": "20000"},{"href":'href2',"price": "20000"},{"href":'href3',"price": "20000"},{"href":'href4',"price": "20000"}]
-dictred = [{"href":'href1',"price": "20000"},{"href":'href2',"price": "20000"},{"href":'href3',"price": "20000"},{"href":'href4',"price": "20000"}]
-dictold = [{"href":'href2',"price": "20000"},{"href":'href3',"price": "20000"},{"href":'href4',"price": "20000"}]
+parsed = [{"href":'href1',"price": "10000"},{"href":'href2',"price": "10000"},{"href":'href3',"price": "15000"},{"href":'href4',"price": "20000"},{"href":'href5',"price": "15000"}]
+dictred = [{"href":'href2',"price": "15000"},{"href":'href5',"price": "15000"}]
+dictold = [{"href":'href3',"price": "20000"},{"href":'href4',"price": "20000"}]
 
 
 
@@ -10,23 +10,123 @@ dict_disc = []
 dict_nochanges = []
 dict_new = []
 
+
 for i in parsed:
     for x in dictred:
         if i['href'] == x['href']:
             if i['price'] < x['price']:
                 dict_disc_again.append(i)        # add to again discounted
-                print(i)
                 continue
             if i['price'] >= x['price']:
                 dict_red_nochanges.append(i)
-                print(i)
                 continue
-        for z in dictold:
+
+parsed1 = []
+for i in dict_disc_again:
+    for x in parsed:
+        if i != x:
+            parsed1.append(x)
+parsed = []
+for i in dict_red_nochanges:
+    for x in parsed1:
+        if i != x:
+            parsed.append(x)
+
+for i in parsed:
+    for z in dictold:
+        if i['href'] == z['href']:
             if i['price'] < z['price']:
                 dict_disc.append(i)
                 continue
             if i['price'] >= z['price']:
                 dict_nochanges.append(i)
                 continue
-            else:
-                dict_new.append(i)
+
+parsed1 = []
+for i in dict_disc:
+    for x in parsed:
+        if i != x:
+            parsed1.append(x)
+parsed = []
+for i in dict_nochanges:
+    for x in parsed1:
+        if i != x:
+            dict_new.append(x)
+
+out_new = dict_nochanges + dict_new
+out_disc = dict_disc+dict_disc_again + dict_red_nochanges
+
+
+def load_to_html():
+    with open('output.html', 'w+') as out_file:
+        if not dict_new:
+            line = "<center><h2> No new cars " + "</center></h2>"
+            out_file.write(line)
+        if dict_new:
+            line = "<center><h2> New cars " + "</center></h2>"
+            out_file.write(line)
+            for i in dict_new:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+        if not dict_disc_again:
+            line = ""
+        else:
+            line = "<center><h2>  Price reduced AGAIN! </center></h2>"
+            out_file.write(line)
+            for i in dict_disc_again:
+                for x in dictred:
+                    if i['href'] == x['href']:
+                        oldprice = x['price']
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "&nbsp;&nbsp;&nbsp;&nbsp;Old price is: " + oldprice + "<br />"
+                out_file.write(line)
+
+        if not dict_disc:
+            line = ""
+        else:
+            line = "<center><h2>  Price reduced </center></h2> "
+            out_file.write(line)
+            for i in dict_disc:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+        if not dict_nochanges:
+            line = ""
+        else:
+            line = "<center><h2>  Old Cars </center></h2>"
+            out_file.write(line)
+            for i in dict_nochanges:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+        if not dict_red_nochanges:
+            line = ""
+        else:
+            for i in dict_red_nochanges:
+                line = 'Link is : <a href="' + i['href'] + '">' + i['href'] + '</a>' + "&nbsp;&nbsp;&nbsp;&nbsp;Price is: " + i['price'] + "<br />"
+                out_file.write(line)
+
+
+load_to_html()
+
+def cli_output():
+    for i in dict_new:
+        print("NEW                  Key = " + i['href'] + "    Price =     " + i['price'])
+
+    for i in dict_disc_again:
+        print("PRICE REDUCED AGAIN! Key = " + i['href'] + "    New price = " + i['price'] + "    Old price is = " + i['price'])
+
+    for i in dict_disc:
+        print("PRICE REDUCED!       Key = " + i['href'] + "    New price = " + i['price'] + "    Old price is = " + i['price'])
+
+    for i in dict_nochanges:
+        print("OLD                  Key = " + i['href'] + "    Value =     " + i['price'])
+
+    for i in dict_red_nochanges:
+        print("OLD                  Key = " + i['href'] + "    Value =     " + i['price'])
+
+print(dict_new)
+print(dict_red_nochanges)
+print(dict_disc_again)
+print(dict_disc)
+print(dict_nochanges)
